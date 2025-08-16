421f5e70638dce52bac5ad67b1f73497b49e52c4
Karol115 <karol20080518@gmail.com>
2025-08-16 22:25:41 +0200

    Add project files.

---
+++ EncodePlus/MainPage.xaml.cs
@@ -0,0 +1,118 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using System.Security.Cryptography;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace EncodePlus
+{
+    public sealed partial class MainPage : Page
+    {
+        public enum OperationType
+        {
+            Encoding,
+            Hash,
+            Hex,
+        }
+
+        public class Codec
+        {
+            public OperationType Type { get; set; }
+            public Func<string, string> Encode { get; set; }
+            public Func<string, string> Decode { get; set; }
+        }
+
+        Dictionary<string, Codec> operations = new Dictionary<string, Codec>
+        {
+            { "Base64", new Codec {
+                Type = OperationType.Encoding,
+                Encode = input => Convert.ToBase64String(Encoding.UTF8.GetBytes(input)),
+                Decode = input => Encoding.UTF8.GetString(Convert.FromBase64String(input))
+            }},
+            { "URL", new Codec {
+                Type = OperationType.Encoding,
+                Encode = input => Uri.EscapeDataString(input),
+                Decode = input => Uri.UnescapeDataString(input)
+            }},
+            { "ASCII", new Codec {
+                Type = OperationType.Encoding,
+                Encode = input => Logic.EncodeInput(Encoding.ASCII, input),
+                Decode = input => Logic.DecodeInput(Encoding.UTF8, input)
+            }},
+            { "Utf-7", new Codec {
+                Type = OperationType.Encoding,
+                Encode = input => Logic.EncodeInput(Encoding.UTF7, input),
+                Decode = input => Logic.DecodeInput(Encoding.UTF7, input)
+            }},
+            { "Utf-8", new Codec {
+                Type = OperationType.Encoding,
+                Encode = input => Logic.EncodeInput(Encoding.UTF8, input),
+                Decode = input => Logic.DecodeInput(Encoding.UTF8, input)
+            }},
+            { "Utf-32", new Codec {
+                Type = OperationType.Encoding,
+                Encode = input => Logic.EncodeInput(Encoding.UTF8, input),
+                Decode = input => Logic.DecodeInput(Encoding.UTF8, input)
+            }},
+            { "Hex", new Codec {
+                Type = OperationType.Hex,
+                Encode = input => Logic.EncodeInput(null, input, true),
+                Decode = input => Logic.DecodeInput(null, input, true)
+            }},
+            { "SHA-1", new Codec {
+                Type = OperationType.Hash,
+                Encode = input => Logic.HashInput(SHA1.Create(), input)
+            }},
+            { "SHA-256", new Codec {
+                Type = OperationType.Hash,
+                Encode = input => Logic.HashInput(SHA256.Create(), input)
+            }},
+            { "SHA-512", new Codec {
+                Type = OperationType.Hash,
+                Encode = input => Logic.HashInput(SHA512.Create(), input)
+            }},
+            { "MD5", new Codec {
+                Type = OperationType.Hash,
+                Encode = input => Logic.HashInput(MD5.Create(), input)
+            }},
+        };
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            AboutPage.addCoCreator("Co-designer: Kierownik223");
+            ComboBox.ItemsSource = operations.Keys;
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxInput.Text = "";
+            TextBoxOutput.Text = "";
+        }
+
+        private void EncodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxOutput.Text = operations[ComboBox.SelectedItem.ToString()].Encode(TextBoxInput.Text);
+        }
+
+        private void DecodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Codec codec = operations[ComboBox.SelectedItem.ToString()];
+            TextBoxOutput.Text = codec != null ? operations[ComboBox.SelectedItem.ToString()].Decode(TextBoxInput.Text) : "Can't decode hash";
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DecodeButton.IsEnabled = (operations[ComboBox.SelectedItem.ToString()].Type != OperationType.Hash);
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataPackage package = new DataPackage();
+            package.SetText(TextBoxOutput.Text);
+            Clipboard.SetContent(package);
+        }
+    }
+}
---
+++ EncodePlus/logic.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace EncodePlus
+{
+    /*public static class UtfASCII
+    {
+        private static Encoding returnEncode(int utfASCII)
+        {
+            switch (utfASCII)
+            {
+                case 0:
+                    return Encoding.ASCII;
+                case 8:
+                    return Encoding.UTF8;
+                case 7:
+                    return Encoding.UTF7;
+                case 32:
+                    return Encoding.UTF32;
+                default:
+                    return Encoding.UTF8;
+            }
+        }
+
+        /// <param name="utfASCII">0-ASCII      7-UTF7      8-UTF8      32-UTF32</param>
+        public static string Encode(string input, int utfASCII)
+        {
+            Encoding encoding = returnEncode(utfASCII);
+        }
+
+        /// <param name="utfASCII">0-ASCII      7-UTF7      8-UTF8      32-UTF32</param>
+        public static string Decode(string input, int utfASCII)
+        {
+            Encoding encoding = returnEncode(utfASCII);
+
+            string[] byteStrings = input.Split(' ');
+            byte[] utf8Bytes = new byte[byteStrings.Length];
+
+            for (int i = 0; i < byteStrings.Length; i++)
+            {
+                if (byte.TryParse(byteStrings[i], out byte b))
+                {
+                    utf8Bytes[i] = b;
+                }
+            }
+
+            return encoding.GetString(utf8Bytes);
+        }
+
+    }*/
+
+    /*public static class Hex
+    {
+        public static string HexToString(string hex)
+        {
+            hex = hex.Replace(" ", "");
+
+            byte[] bytes = new byte[hex.Length / 2];
+
+            for (int i = 0; i < hex.Length; i += 2)
+            {
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            }
+
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        public static string StringToHex(string input)
+        {
+            return BitConverter.ToString(Encoding.UTF8.GetBytes(input)).Replace("-", "").ToLower();
+        }
+    }*/
+
+    public static class Logic
+    {
+        public static string HashInput(HashAlgorithm hash, string input)
+        {
+            return BitConverter.ToString(hash.ComputeHash(Encoding.UTF8.GetBytes(input)))
+                .Replace("-", "").ToLower();
+        }
+
+        public static string EncodeInput(Encoding encoding, string input, bool hex = false)
+        {
+            if (!hex)
+            {
+                byte[] utf8Bytes = encoding.GetBytes(input);
+                StringBuilder encoded = new StringBuilder();
+
+                foreach (byte b in utf8Bytes)
+                {
+                    encoded.Append(b);
+                    encoded.Append(" ");
+                }
+
+                return encoded.ToString().Trim();
+            }
+            else
+            {
+                return BitConverter.ToString(Encoding.UTF8.GetBytes(input)).Replace("-", "").ToLower();
+            }
+        }
+
+        public static string DecodeInput(Encoding encoding, string input, bool hex = false)
+        {
+            if (!hex)
+            {
+                byte[] utf8Bytes = encoding.GetBytes(input);
+                StringBuilder encoded = new StringBuilder();
+
+                foreach (byte b in utf8Bytes)
+                {
+                    encoded.Append(b);
+                    encoded.Append(" ");
+                }
+
+                return encoded.ToString().Trim();
+            }
+            else
+            {
+                string hexStr = input.Replace(" ", "");
+
+                byte[] bytes = new byte[hexStr.Length / 2];
+
+                for (int i = 0; i < hexStr.Length; i += 2)
+                {
+                    bytes[i / 2] = Convert.ToByte(hexStr.Substring(i, 2), 16);
+                }
+
+                return Encoding.UTF8.GetString(bytes);
+            }
+        }
+    }
+}

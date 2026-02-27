12655be7597aa126512a0eb047b10c644fae5c7c
Karol115 <karol@marmak.net.pl>
2026-02-27 23:55:45 +0100

    AboutPage Update

--- EncodePlus/Properties/AssemblyInfo.cs
+++ EncodePlus/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EncodePlus")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © 2024-2026 Karol115 All rights reserved")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
